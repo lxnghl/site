e9ec286fc4d0df727e5f3e51c2e7fcd41fb0502b
Alex Anghel <alex@alexanghel.com>
2024-10-22 00:37:33 +0300

    try again

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
-          },
-        ],
-      },
-    ];
-  },
+  output: "export"
 };
 
 export default nextConfig;
